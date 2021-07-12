04d1039bab25eda0fde1b0b088592a21dd5b099d
TsekNet <admin@tseknet.com>
2021-07-12 18:53:51 -0400

    fix: Add -UseBasicParsing

--- dot_config/profile.ps1
+++ dot_config/profile.ps1
@@ -83,7 +83,7 @@ function sudo {
 ################################################################################
 # Update the console title with current PowerShell elevation and version       #
 ################################################################################
-$Host.UI.RawUI.WindowTitle = "PS | v$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor) | $((Invoke-WebRequest wttr.in/new_york_city?format="%c%t&u").content)"
+$Host.UI.RawUI.WindowTitle = "PS | v$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor) | $((Invoke-WebRequest wttr.in/new_york_city?format="%c%t&u" -UseBasicParsing).content)"
 
 ################################################################################
 # PSReadLine and prompt options                                                #
