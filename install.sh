d745c19414709587a2a5ed3635768d589770fd10
Dan Tsekhanskiy <admin@tseknet.com>
2023-12-03 15:04:43 -0500

    fix: Comment out URL

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
-https://code.visualstudio.com/docs/setup/linux
+# https://code.visualstudio.com/docs/setup/linux
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
